08db5f7887e771d9790eb3ba5766186ca94cd51e
InsanityCode <56584762+InsanityCode@users.noreply.github.com>
2024-03-25 21:34:27 +0100

    fix leading path separator preservation

--- source/Paths/GlobRegex.cs
+++ source/Paths/GlobRegex.cs
@@ -81,7 +81,9 @@ namespace ChaosUtil.Platform.Paths
         public static string ConvertGlobToRegex(string glob)
         {
             Match firstPathSeparator = pathSeparatorRegex.Match(glob);
-            bool startsWithPathSeparator = firstPathSeparator != null && firstPathSeparator.Index == 0;
+            bool startsWithPathSeparator = firstPathSeparator != null
+                && firstPathSeparator.Success
+                && firstPathSeparator.Index == 0;
 
             // reduce sequences like 'a/**/**/b' to 'a/**/b', to make path separator handling easier
             bool hasMultiDoubleAsterisk = true;
