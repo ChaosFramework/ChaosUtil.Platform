1189e229aa6dd3e9925592d985b3573cc6910139
InsanityCode <56584762+InsanityCode@users.noreply.github.com>
2024-04-04 19:12:16 +0200

    support 32bppArgb thumbnails

--- source/Windows/Thumbnail/WindowsThumbnailProvider.cs
+++ source/Windows/Thumbnail/WindowsThumbnailProvider.cs
@@ -8,6 +8,7 @@
 
 using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Runtime.InteropServices;
 
@@ -30,7 +31,28 @@ namespace ChaosUtil.Platform.Windows.Thumbnail
 
             try
             {
-                return Image.FromHbitmap(hBitmap);
+                Bitmap bm = Image.FromHbitmap(hBitmap);
+                switch (bm.PixelFormat)
+                {
+                    case PixelFormat.Format32bppRgb:
+                        {
+                            /* From https://learn.microsoft.com/en-us/dotnet/api/system.drawing.imaging.pixelformat
+                             *     Specifies that the format is 32 bits per pixel;
+                             *     8 bits each are used for the red, green, and blue components.
+                             *     The remaining 8 bits are not used.
+                             *
+                             * Apparently in this case the unused 8 bits actually hold a valid alpha channel...
+                             */
+
+                            Bitmap argb = ConvertRGB32toARGB32(bm);
+                            bm.Dispose();
+                            return argb;
+                        }
+
+                    default:
+                        // TODO: Figure out whether there are more formats that actually contain alpha information.
+                        return bm;
+                }
             }
             finally
             {
@@ -101,6 +123,35 @@ namespace ChaosUtil.Platform.Windows.Thumbnail
                     return bitmap.GetHbitmap();
                 }
             }
+        }
+
+        unsafe static Bitmap ConvertRGB32toARGB32(Bitmap rgb32)
+        {
+            const PixelFormat RGB32 = PixelFormat.Format32bppRgb;
+            const PixelFormat ARGB32 = PixelFormat.Format32bppArgb;
+
+            if (rgb32.PixelFormat != RGB32)
+                throw new ArgumentException($"The provided bitmap must have the {RGB32} format.", nameof(rgb32));
+
+            Bitmap argb32 = new Bitmap(rgb32.Width, rgb32.Height, ARGB32);
+
+            Rectangle bounds = new Rectangle(0, 0, rgb32.Width, rgb32.Height);
+            BitmapData scanRgb = rgb32.LockBits(bounds, ImageLockMode.ReadOnly, RGB32);
+            BitmapData scanArgb = argb32.LockBits(bounds, ImageLockMode.WriteOnly, ARGB32);
+
+            int bytesPerRow = 4 * scanRgb.Width;
+            for (int xOffset = 0; xOffset < bytesPerRow; xOffset += 4)
+                for (int row = 0; row < scanRgb.Height; row++)
+                {
+                    IntPtr rgbPos = IntPtr.Add(scanRgb.Scan0, xOffset + row * scanRgb.Stride);
+                    IntPtr argbPos = IntPtr.Add(scanArgb.Scan0, xOffset + row * scanArgb.Stride);
+                    *(int*)argbPos = *(int*)rgbPos;
+                }
+
+            rgb32.UnlockBits(scanRgb);
+            argb32.UnlockBits(scanArgb);
+
+            return argb32;
         }
     }
 }
