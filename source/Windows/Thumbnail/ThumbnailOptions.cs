7ce8b978d8b5701483bd8abdcd57f8496178c809
InsanityCode <56584762+InsanityCode@users.noreply.github.com>
2024-04-22 17:04:28 +0200

    add missing documentation

--- source/Windows/Thumbnail/ThumbnailOptions.cs
+++ source/Windows/Thumbnail/ThumbnailOptions.cs
@@ -10,14 +10,26 @@ using System;
 
 namespace ChaosUtil.Platform.Windows.Thumbnail
 {
+    /// <summary> Control whether or how a loaded thumbnail is scaled and whether it may be loaded from memory, cache or disk. </summary>
     [Flags]
     public enum ThumbnailOptions
     {
+        /// <summary> Scale down to fit the desired size without changing aspect ratio. </summary>
         RESIZETOFIT = 0x00,
+
+        /// <summary> Specifies that the resulting thumbnail may be larger than the desired size. </summary>
         BiggerSizeOk = 0x01,
+
+        /// <summary> Only return a thumbnail if it's already loaded in memory. </summary>
         InMemoryOnly = 0x02,
+
+        /// <summary> Only return icons. </summary>
         IconOnly = 0x04,
+
+        /// <summary> Only return thumbnails. </summary>
         ThumbnailOnly = 0x08,
+
+        /// <summary> Only fetch from disk if the icon is already cached. </summary>
         InCacheOnly = 0x10,
     }
 }
--- source/Windows/Thumbnail/WindowsThumbnailProvider.cs
+++ source/Windows/Thumbnail/WindowsThumbnailProvider.cs
@@ -14,8 +14,23 @@ using System.Runtime.InteropServices;
 
 namespace ChaosUtil.Platform.Windows.Thumbnail
 {
+    /// <summary> Provides functionality to load a thumbnail for a file. </summary>
     public static class WindowsThumbnailProvider
     {
+        /// <summary> Returns a <see cref="Bitmap"/> containing the thumbnail for the given <paramref name="fileName"/>. </summary>
+        /// <param name="fileName"> The path to the file to retrieve the thumbnail for. </param>
+        /// <param name="width">
+        ///     The desired width of the thumbnail.
+        ///     Depending on <paramref name="options"/> the actual width may differ.
+        /// </param>
+        /// <param name="height">
+        ///     The desired height of the thumbnail.
+        ///     Depending on <paramref name="options"/> the actual height may differ.
+        /// </param>
+        /// <param name="options">
+        ///     Control whether or how a loaded thumbnail is scaled
+        ///     and whether it may be loaded from memory, cache or disk.
+        /// </param>
         public static Bitmap GetThumbnail(string fileName, int width, int height, ThumbnailOptions options)
         {
             IntPtr hBitmap = IntPtr.Zero;
